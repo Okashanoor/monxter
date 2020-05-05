6f892f46273afe6dff00c1c82304e38d54e1e165
Okashanoor <64841437+Okashanoor@users.noreply.github.com>
2020-05-05 17:11:48 +0500

    Update and rename zeroboy.py to shinwariboy.py

--- shinwariboy.py
+++ shinwariboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - OKASHA -OFFICAL------•◈•
+|__\~~) •||•SHINWARI - BOY---------------•◈•
+|__(-----\  •◈•------BLACK-MAFIA- GANG--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mOKASHA NOOR
+\033[1;96mInstagram \033[1;93m: \033[1;FlowOKASHANOOR
+\033[1;96mFacebook  \033[1;93m: \033[1; OKASHANOOR007
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/TheOkasha/monxter
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mokasha noor
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowokasha
+\033[1;96mFacebook  \033[1;93m: \033[1;92m okasha007
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Theokasha/monxter
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "nomi"
+CorrectPassword = "okasha"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,11 +100,12 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.facebook.com/100036969377018
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.Facebook.com/100036969377018
 
+	
 def login():
 	os.system('clear')
 	try:
